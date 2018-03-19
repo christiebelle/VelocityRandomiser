669201ebaced38cec57f0fda73d5c2d592fba46c
Christiebelle <cslester@outlook.com>
2018-03-19 18:48:03 +0000

    Controller set up

--- src/main/java/RandomiserController.java
+++ src/main/java/RandomiserController.java
@@ -1,2 +1,20 @@
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import static spark.Spark.get;
+import static spark.SparkBase.staticFileLocation;
+
 public class RandomiserController {
+
+        public static void main(String[] args) {
+            VelocityTemplateEngine velocityTemplateEngine = new VelocityTemplateEngine();
+            staticFileLocation("/public");
+
+            get("/one", (req, res) -> {}, velocityTemplateEngine);
+
+        }
+    }
 }
